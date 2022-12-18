606b82f642d97496d0417605df8ba8c8f93096e6
pisjad <hafizhassajjad17@gmail.com>
2022-12-19 03:46:19 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["*.{html,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                teks: ["Montserrat"],
+            },
+        },
+    },
+    plugins: [],
+};
